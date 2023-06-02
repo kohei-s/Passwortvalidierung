f89c89133178e8808c9bc8c1f338741753c7e2d4
Kohei Suzuki <koheisuzuki@me.com>
2023-06-02 14:02:54 +0200

    Second commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,75 @@
+import java.util.Scanner;
 public class Main {
+
+    public static void main(String[] args) {
+        String passwort = userInput();
+        isRange(passwort);
+        isNumberInPass(passwort);
+        isLowerCaseInPass(passwort);
+        isUpperCaseInPass(passwort);
+    }
+
+    public static String userInput() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Bitte Passwort eingeben:");
+        String passwort = scanner.nextLine();
+        System.out.println("Dein Passwort ist: " + passwort);
+        return passwort;
+    }
+
+    public static boolean isRange (String passwort){
+        int passLength = passwort.length();
+        System.out.println("Die Länge deines Passworts ist: " + passLength);
+        boolean result = (passLength >= 8) && (passLength <= 20);
+        System.out.println("Die Länge ist gültig: " + result);
+        return result;
+   }
+   public static boolean isNumberInPass(String passwort){
+       int count = 0;
+       for(int i = 0; i < passwort.length(); i++) {
+           if(Character.isDigit(passwort.charAt(i))) {
+               count++;
+           }
+       }
+       boolean result = count > 0;
+       System.out.println("Das Passwort enthält " + ((result)? "":"keine ") + "Zahlen");
+       return result;
+
+    }
+    public static boolean isLowerCaseInPass (String passwort) {
+        int count = 0;
+        for(int i = 0; i < passwort.length(); i++) {
+            if(Character.isLowerCase(passwort.charAt(i))) {
+                count++;
+            }
+        }
+        boolean result = count > 0;
+        System.out.println("Das Passwort enthält " + ((result)? "kleine":"keine kleinen") + " Buchstaben");
+        return result;
+    }
+
+    public static boolean isUpperCaseInPass (String passwort) {
+        int count = 0;
+        for(int i = 0; i < passwort.length(); i++) {
+            if(Character.isUpperCase(passwort.charAt(i))) {
+                count++;
+            }
+        }
+        boolean result=  count > 0;
+        System.out.println("Das Passwort enthält " + ((result)? "":"keine ") + "Buchstaben");
+        return result;
+    }
+    public static boolean isValidPass(String passwort) {
+        int count = 0;
+        String[] invalidPass = {"123456", "passwort"};
+        for (String pass : invalidPass) {
+            if (passwort == pass) {
+                System.out.println("Das Passwort ist schlecht");
+                count++;
+            }
+        }
+        boolean result = count > 0;
+        System.out.println("Das Passwort ist " + ((result)? "schlecht":"gut"));
+        return result;
+    }
 }
--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,2 +1,74 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
 public class MainTest {
+
+//    @Test
+//    void inputTest () {
+//        String result = Main.userInput();
+//        assertNotNull(result);
+//    }
+
+    @Test
+    void isRangeTest (){
+        boolean actual = Main.isRange("12345678");
+        assertTrue(actual);
+    }
+
+    @Test
+    void isRangeTest2 (){
+        boolean actual = Main.isRange("12345");
+        assertFalse(actual);
+    }
+
+
+    @Test
+    void isNumberInPassTest () {
+        boolean actual = Main.isNumberInPass("12345678");
+        assertTrue(actual);
+    }
+
+    @Test
+    void isNumberInPassTest2 () {
+        boolean actual = Main.isNumberInPass("abcdefg");
+        assertFalse(actual);
+    }
+
+    @Test
+    void isLowerCaseInPassTest () {
+        boolean actual = Main.isLowerCaseInPass("ABCDEFGHI");
+        assertFalse(actual);
+    }
+
+    @Test
+    void isLowerCaseInPassTest2 () {
+        boolean actual = Main.isLowerCaseInPass("ABCDEFghHI");
+        assertTrue(actual);
+    }
+
+    @Test
+    void isUpperCaseInPassTest () {
+        boolean actual = Main.isUpperCaseInPass("ABCDEFGHI");
+        assertTrue(actual);
+    }
+    @Test
+    void isUpperCaseInPassTest2 () {
+        boolean actual = Main.isUpperCaseInPass("abcdefghi");
+        assertFalse(actual);
+    }
+
+    @Test
+    void isValidPassTest () {
+        boolean actual = Main.isValidPass("123456");
+        assertTrue(actual);
+    }
+
+    @Test
+    void isValidPassTest2 () {
+        boolean actual = Main.isValidPass("fghj46587tZ");
+        assertFalse(actual);
+    }
+
 }
